ecb2cdd1b15d85e68dd824bd300c5ea093add12e
yannakode <yanann1997@gmail.com>
2023-05-23 20:43:04 -0300

    desafio

---
+++ Resposta do desafio em C#/Resolução_Desafio/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Resolução_Desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Pergunta:
+            //P4 - As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12.
+            //Escreva um algoritmo (sequência de passos) que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
+            double ValorTotal = 0;
+            Console.WriteLine("Cálculo maçãs");
+            Console.WriteLine("Digite a quantidade de maçãs que deseja comprar.");
+            int quant = int.Parse(Console.ReadLine());
+            if( quant < 12 )
+            {
+                ValorTotal = quant;
+            }
+            else if( quant >= 12 )
+            {
+                ValorTotal = quant * 1.3;
+            }
+            else
+            {
+                Console.WriteLine("Valor nulo.");
+            }
+            Console.WriteLine("O valor total é: " + ValorTotal);
+
+
+        }
+    }
+}
